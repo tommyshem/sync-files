c880997bb57caf7e43bf9e9e8ed6d6a72972181e
tommyshem <1952659+tommyshem@users.noreply.github.com>
2019-12-08 19:08:48 +0000

    delete target contents

--- rust-sync/target/debug/build/byteorder-9fc5f5c27018a961/build_script_build-9fc5f5c27018a961.d
+++
@@ -1,5 +0,0 @@
-/Users/tommyshemwell/git-workspace/my-github/sync-files/rust-sync/target/debug/build/byteorder-9fc5f5c27018a961/build_script_build-9fc5f5c27018a961: /Users/tommyshemwell/.cargo/registry/src/github.com-1ecc6299db9ec823/byteorder-1.3.2/build.rs
-
-/Users/tommyshemwell/git-workspace/my-github/sync-files/rust-sync/target/debug/build/byteorder-9fc5f5c27018a961/build_script_build-9fc5f5c27018a961.d: /Users/tommyshemwell/.cargo/registry/src/github.com-1ecc6299db9ec823/byteorder-1.3.2/build.rs
-
-/Users/tommyshemwell/.cargo/registry/src/github.com-1ecc6299db9ec823/byteorder-1.3.2/build.rs:
--- rust-sync/target/debug/deps/rust_sync-938d9f84d20b6848.d
+++
@@ -1,5 +0,0 @@
-/Users/tommyshemwell/git-workspace/my-github/sync-files/rust-sync/target/debug/deps/rust_sync-938d9f84d20b6848: src/main.rs
-
-/Users/tommyshemwell/git-workspace/my-github/sync-files/rust-sync/target/debug/deps/rust_sync-938d9f84d20b6848.d: src/main.rs
-
-src/main.rs:

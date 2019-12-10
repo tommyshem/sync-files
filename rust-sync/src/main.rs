b7f149638def59f0b6df2f28ca889feba9d8bf0c
tommyshem <1952659+tommyshem@users.noreply.github.com>
2019-12-10 16:22:59 +0000

    changed names to get rid of warnings

--- rust-sync/src/main.rs
+++ rust-sync/src/main.rs
@@ -3,33 +3,32 @@ use std::fs::File;
 use std::io;
 
 fn main() {
-    let stringToHash = "Hello World!\n";
-    hashString(stringToHash);
-    hashFile("../test-files/file.txt")
+    let string_to_hash = "Hello World!\n";
+    hash_string(string_to_hash);
+    hash_file("../test-files/file.txt")
 }
 
-fn hashFile(filePath: &str) {
-    let mut f = File::open(&filePath);
-    let mut f = match f {
+fn hash_file(file_path: &str) {
+    let mut f = match File::open(&file_path) {
         Ok(file) => file,
         Err(error) => panic!("Problem opening the file: {:?}", error),
     };
 
     let mut sha = Sha512::new();
     let n = io::copy(&mut f, &mut sha).expect("error");
-    // Note that calling `result()` consumes hasher
+    // Note that calling `result()` consumes sha
     let hash = sha.result();
-    println!("Path: {}", filePath);
+    println!("Path: {}", file_path);
     println!("Bytes processed: {}", n);
     println!("Hash value: {:x}", hash);
     //return hash;
 }
 
-fn hashString(datatohash: &str) {
+fn hash_string(data_to_hash: &str) {
     //-> [u8; 64] {
     let mut sha = Sha512::new();
-    sha.input(datatohash);
-    // Note that calling `result()` consumes hasher
+    sha.input(data_to_hash);
+    // Note that calling `result()` consumes sha
     let hash = sha.result();
     println!("{:x}", hash);
     //let res: [u8; 64] = sha.result().into()
